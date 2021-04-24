b838a656ae2ef02c522791ef1bc03eda004efac4
prajapatilalit <prajapatilalit150992@gmail.com>
2021-04-25 00:38:55 +0530

    update route of read all students and student by id

--- src/app.js
+++ src/app.js
@@ -11,17 +11,54 @@ app.get("/", (req, res) => {
   res.send("hello welcome to this website");
 });
 
-app.post("/students", (req, res) => {
-  console.log(req.body);
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+//Create Students
+// app.post("/students", (req, res) => {
+//   console.log(req.body);
+//   const user = new Student(req.body);
+//   user
+//     .save()
+//     .then(() => {
+//       res.status(201).send(user);
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
+
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+//Read all students
+
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.status(200).send(studentsData);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+//Read student by _id
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    if (!studentData) {
+      return res.status(404).send("Student Not Found");
+    } else {
+      return res.send(studentData);
+    }
+  } catch (err) {
+    res.send(500).send(err);
+  }
 });
 
 app.listen(port, () => {
